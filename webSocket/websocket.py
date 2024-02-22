36d9764b2f190bbcfe989db33c5e6b3aa8088079
Cristhian-Leon1 <cristhian.leon@unipamplona.edu.co>
2024-02-21 19:08:58 -0500

    Error se desconecta

--- webSocket/websocket.py
+++ webSocket/websocket.py
@@ -1,5 +1,7 @@
 from typing import List
 from fastapi import WebSocket
+from starlette.websockets import WebSocketDisconnect
+
 
 # Variable booleana inicial
 estado = False
@@ -7,9 +9,11 @@ estado = False
 # Lista de clientes conectados
 clientes: List[WebSocket] = []
 
+
 # Función para enviar el estado actual a todos los clientes conectados
 async def enviar_estado(ws: WebSocket):
     await ws.send_json({"estado": estado})
+
 
 async def websocket_endpoint(websocket: WebSocket):
     global estado
@@ -20,15 +24,19 @@ async def websocket_endpoint(websocket: WebSocket):
     # Enviar el estado actual al cliente cuando se conecta
     await enviar_estado(websocket)
 
-    while True:
-        data = await websocket.receive_text()
-        print('Mensaje recibido:', data) # Imprimir el mensaje recibido en consola
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print('Mensaje recibido:', data)  # Imprimir el mensaje recibido en consola
 
-        # Cambiar el estado de acuerdo al mensaje recibido
-        estado = not estado
+            # Cambiar el estado de acuerdo al mensaje recibido
+            estado = not estado
 
-        print('estado:', estado)
+            print('estado:', estado)
 
-        # Enviar el nuevo estado a todos los clientes
-        for cliente in clientes:
-            await enviar_estado(cliente)
+            # Enviar el nuevo estado a todos los clientes
+            for cliente in clientes:
+                await enviar_estado(cliente)
+    except WebSocketDisconnect:
+        print('Cliente desconectado')
+        clientes.remove(websocket)
