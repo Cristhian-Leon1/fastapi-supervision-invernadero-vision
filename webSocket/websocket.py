770c44ed96ac005fb0d507bed08a69ba4afed89d
Cristhian-Leon1 <cristhian.leon@unipamplona.edu.co>
2024-03-04 10:36:55 -0500

    socket con valvula 2

--- webSocket/websocket.py
+++ webSocket/websocket.py
@@ -2,8 +2,9 @@ from typing import List
 from fastapi import WebSocket
 from starlette.websockets import WebSocketDisconnect
 
-# Variable booleana inicial
-estado = False
+# Variables booleanas iniciales
+estado_valvula1 = False
+estado_valvula2 = False
 
 # Lista de clientes conectados
 clientes: List[WebSocket] = []
@@ -11,11 +12,11 @@ clientes: List[WebSocket] = []
 
 # Función para enviar el estado actual a todos los clientes conectados
 async def enviar_estado(ws: WebSocket):
-    await ws.send_json({"estado": estado})
+    await ws.send_json({"estado_valvula1": estado_valvula1, "estado_valvula2": estado_valvula2})
 
 
 async def websocket_endpoint(websocket: WebSocket):
-    global estado
+    global estado_valvula1, estado_valvula2
     await websocket.accept()
     clientes.append(websocket)
     print('Cliente conectado')
@@ -29,15 +30,21 @@ async def websocket_endpoint(websocket: WebSocket):
             print('Mensaje recibido:', data)
 
             if data == 'Encender valvula 1':
-                estado = True
+                estado_valvula1 = True
             elif data == 'Apagar valvula 1':
-                estado = False
+                estado_valvula1 = False
+            elif data == 'Encender valvula 2':
+                estado_valvula2 = True
+            elif data == 'Apagar valvula 2':
+                estado_valvula2 = False
 
-            print('estado:', estado)
+            print('estado valvula 1:', estado_valvula1)
+            print('estado valvula 2:', estado_valvula2)
 
             for cliente in clientes:
                 await enviar_estado(cliente)
     except WebSocketDisconnect:
         print('Cliente desconectado')
         clientes.remove(websocket)
-        estado = False
+        estado_valvula1 = False
+        estado_valvula2 = False
